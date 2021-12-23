6fa0ae4c3f1ef2173f89cb72d8ccd2d6f53619e6
Amr Moataz <amo@esignatur.dk>
2021-12-23 15:42:27 +0200

    add ip addresss configuration

--- WI_Share/iot/iot.ino
+++ WI_Share/iot/iot.ino
@@ -17,6 +17,7 @@ const char* password = "123456789";
 const char* mqtt_server = "51.15.236.147";
 const char* SSID_INPUT = "ssid_input";
 const char* PASSWORD_INPUT = "password_input";
+const char* IP_INPUT = "ip_input";
 String ssidConfigurationValue = "";
 String passwordConfigurationValue = "";
 
@@ -40,6 +41,10 @@ const char Startup_Configuration[] PROGMEM = R"rawliteral(
       color: white;
       broder-radius: 7px;
       border: none
+    }
+
+    .p-input {
+      font-size: small;
     }
 
     .header {
@@ -79,14 +84,18 @@ const char Startup_Configuration[] PROGMEM = R"rawliteral(
   <div class= "container">
   <h1 class="header">ESP32 Configuration</h1>
   <div class="config-container">
-  <p>Please enter the ssid and password for the network you want to configure.</p>
    <form action="/get">
-   <p>ssid: </p>
+   <p>Please enter the ssid and password for the network you want to configure.</p>
+   <p class="p-input">ssid: </p>
     <input type="text" name="ssid_input">
     <br>
-    <p>password: </p>
+    <p class="p-input">password: </p>
     <input type="password" name="password_input">
     <br>
+   <p>Enter the Message Broker IP Address.</p>
+   <p class="p-input"> IP Address: </P>
+   <input type="text" name="ip_input">
+   <br>
     <input class="btn primary" type="submit" value="Submit">
   </form>
   </div>
@@ -205,23 +214,34 @@ void setupCofigurationPage(){
     String ssidInputParam = "N/A";
     String passwordInputMessage = "N/A";
     String passwordInputParam = "N/A";
+    String ipInputMessage = "N/A";
+    String ipInputParam = "N/A";
   configured = true;
     
-    // GET input1 value on <ESP_IP>/get?input1=<inputMessage>
+    // GET ssid value on <ESP_IP>/get?input1=<inputMessage>
     if (request->hasParam(SSID_INPUT)) {
       ssidInputMessage = request->getParam(SSID_INPUT)->value();
       ssidInputParam = SSID_INPUT;
     }
-    // GET input2 value on <ESP_IP>/get?input2=<inputMessage>
+    // GET password value on <ESP_IP>/get?input2=<inputMessage>
     if (request->hasParam(PASSWORD_INPUT)) {
       passwordInputMessage = request->getParam(PASSWORD_INPUT)->value();
       passwordInputParam = PASSWORD_INPUT;
     }
+
+    // GET ip address value on <ESP_IP>/get?input2=<inputMessage>
+    if (request->hasParam(IP_INPUT)) {
+      ipInputMessage = request->getParam(IP_INPUT)->value();
+      ipInputParam = PASSWORD_INPUT;
+    }
+    
     Serial.println(ssidInputMessage);
     Serial.println(passwordInputMessage);
+    Serial.println(ipInputMessage);
     request->send(200, "text/html", "HTTP GET request sent to your ESP on input field (" 
                                      + ssidInputParam + ") with value: " + ssidInputMessage +
                                      " and input field (" + passwordInputParam + ") with value: " + passwordInputMessage +
+                                     " and input field (" + ipInputParam + ") with value: " + ipInputMessage +
                                      "<br><a href=\"/\">Return to Home Page</a>");
   });
 }
