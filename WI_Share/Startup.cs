6b565210eb01c4437be68e02aa57481964ae1939
Fady Taher <fta@luftborn.com>
2021-12-23 17:48:55 +0200

    Setting

--- WI_Share/Startup.cs
+++ WI_Share/Startup.cs
@@ -56,7 +56,7 @@ namespace WI_Share
 			{
 				endpoints.MapControllerRoute(
 					name: "default",
-					pattern: "{controller=Home}/{action=Index}/{id?}");
+					pattern: "{controller=Home}/{action=Setting}/{id?}");
 				//endpoints.MapRazorPages();
 			});
 		}
